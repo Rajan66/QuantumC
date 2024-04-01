883449d6c2e54d180a020b5f25e8c8f5f005d338
Rajan66 <rajanmaharjan042@gmail.com>
2024-04-02 01:43:23 +0545

    fix: navbar

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,13 +4,12 @@ import Navbar from './Navbar'
 
 const Hero = () => {
     return (
-        <section style={{ backgroundImage: `url(${coverimg})` }} className='bg-center h-[70vh] bg-cover'>
-            <div className="relative text-white py-12 md:py-24 md:px-8 text-center">
-                <div className="relative z-10">
-                    <h1 className="text-4xl md:text-6xl font-bold mb-2 md:mb-4">Striving for Progress</h1>
-                    <h1 className="text-2xl md:text-4xl font-bold mb-2 md:mb-4">Leading Towards Perfection</h1>
-                    <p className="text-sm md:text-lg">You understand your customers better than anyone. We know that, and we can help you turn that knowledge into results.</p>
-                </div>
+        <section className='bg-center h-[70vh] bg-cover relative'>
+            <div className="absolute inset-0 bg-cover bg-center brightness-50" style={{ backgroundImage: `url(${coverimg})` }}></div>
+            <div className="relative text-white py-12 md:py-24 md:px-8 text-center z-10">
+                <h1 className="text-4xl md:text-6xl font-bold mb-4 md:mb-10">Striving for Progress</h1>
+                <h1 className="text-4xl md:text-6xl font-bold mb-4 md:mb-10">Leading Towards Perfection</h1>
+                <p className="text-medium md:text-lg">You understand your customers better than anyone. We know that, and we can help you turn that knowledge into results.</p>
             </div>
         </section>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,29 +4,27 @@ import coverimg from '../images/hero.jpg'
 
 const Navbar = () => {
     return (
-        <nav className="flex items-center justify-between p-6 lg:px-8">
+        <nav className="flex items-center justify-between lg:mx-[400px] p-6 lg:px-8">
             <div className="flex lg:flex-1">
                 <a href="/" className="-m-1.5 p-1.5">
-                    <img className="h-8 w-auto" src={logo} alt="" />
+                    <img className="h-14 w-auto" src={logo} alt="" />
                 </a>
             </div>
-            <div className="flex lg:hidden">
+            <div className="flex  lg:hidden">
                 <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                    <svg className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
                 </button>
             </div>
-            <div class="hidden lg:flex lg:gap-x-12">
-                <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Home</a>
-                <a href="/" class="text-sm font-semibold leading-6 text-gray-900">About us</a>
-                <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Services</a>
-                <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Portfolio</a>
-                <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Contact Us</a>
+            <div class="hidden text-md lg:flex lg:gap-x-12">
+                <a href="/" class=" font-semibold leading-6 text-gray-900">Home</a>
+                <a href="/" class=" font-semibold leading-6 text-gray-900">About us</a>
+                <a href="/" class=" font-semibold leading-6 text-gray-900">Services</a>
+                <a href="/" class=" font-semibold leading-6 text-gray-900">Portfolio</a>
+                <a href="/" class="font-semibold leading-6 text-gray-900">Contact Us</a>
             </div>
-            <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-                <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
-            </div>
+
         </nav>
 
     )
