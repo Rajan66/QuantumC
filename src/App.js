32bf444c18669e624c36990fbae68598d0a5344e
Rajan66 <rajanmaharjan042@gmail.com>
2024-04-02 01:17:00 +0545

    designing the hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Footer from "./components/Footer";
-import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="">
-      <Header />
+    <div className="App">
+      <Navbar/>
+      <Hero />
       <Footer />
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,32 @@
 import React from 'react'
+import logo from '../images/logo.png'
+import coverimg from '../images/hero.jpg'
 
 const Navbar = () => {
     return (
-
-        <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-            <div class="flex lg:flex-1">
-                <a href="/" class="-m-1.5 p-1.5">
-                    <span class="sr-only">Quantum Compilers</span>
-                    <img class="h-8 w-auto" src="" alt="" />
+        <nav className="flex items-center justify-between p-6 lg:px-8">
+            <div className="flex lg:flex-1">
+                <a href="/" className="-m-1.5 p-1.5">
+                    <img className="h-8 w-auto" src={logo} alt="" />
                 </a>
             </div>
-            <div class="flex lg:hidden">
-                <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <div className="flex lg:hidden">
+                <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
                 </button>
             </div>
             <div class="hidden lg:flex lg:gap-x-12">
-                <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Product</a>
-                <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Features</a>
-                <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Marketplace</a>
-                <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Company</a>
+                <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Home</a>
+                <a href="/" class="text-sm font-semibold leading-6 text-gray-900">About us</a>
+                <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Services</a>
+                <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Portfolio</a>
+                <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Contact Us</a>
             </div>
-            {/* <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+            <div class="hidden lg:flex lg:flex-1 lg:justify-end">
                 <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
-            </div> */}
+            </div>
         </nav>
 
     )
