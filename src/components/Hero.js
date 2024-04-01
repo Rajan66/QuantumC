d762f653b005d465af284f4100ec99623e2c5c1a
Rajan66 <rajanmaharjan042@gmail.com>
2024-04-02 02:00:29 +0545

    ft:added button in hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,10 +6,26 @@ const Hero = () => {
     return (
         <section className='bg-center h-[70vh] bg-cover relative'>
             <div className="absolute inset-0 bg-cover bg-center brightness-50" style={{ backgroundImage: `url(${coverimg})` }}></div>
-            <div className="relative text-white py-12 md:py-24 md:px-8 text-center z-10">
-                <h1 className="text-4xl md:text-6xl font-bold mb-4 md:mb-10">Striving for Progress</h1>
-                <h1 className="text-4xl md:text-6xl font-bold mb-4 md:mb-10">Leading Towards Perfection</h1>
-                <p className="text-medium md:text-lg">You understand your customers better than anyone. We know that, and we can help you turn that knowledge into results.</p>
+            <div className="relative text-white py-12  lg:py-24 lg:px-8 text-center z-10">
+                <h1 className="mt-10 text-4xl lg:text-6xl font-bold mb-6 lg:mb-10">Striving for Progress</h1>
+                <h1 className="text-4xl lg:text-6xl font-bold mb-6 lg:mb-10">Leading Towards Perfection</h1>
+                <p className="text-medium lg:text-lg">You understand your customers better than anyone. We know that, and we can help you turn that knowledge into results.</p>
+
+                <div className='flex mt-[50px] justify-center  '>
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-6 rounded-full mx-10 flex items-center">
+                        <span>Create with Us</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M5 10a1 1 0 011-1h6.586l-2.293-2.293a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H6a1 1 0 01-1-1z" clipRule="evenodd" />
+                        </svg>
+                    </button>
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-6 rounded-full mx-10 flex items-center">
+                        <span>Join Us</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M5 10a1 1 0 011-1h6.586l-2.293-2.293a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H6a1 1 0 01-1-1z" clipRule="evenodd" />
+                        </svg>
+                    </button>
+
+                </div>
             </div>
         </section>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,13 @@ import coverimg from '../images/hero.jpg'
 
 const Navbar = () => {
     return (
-        <nav className="flex items-center justify-between lg:mx-[400px] p-6 lg:px-8">
+        <nav className="flex items-center justify-between lg:mx-[100px] p-6 lg:px-8">
             <div className="flex lg:flex-1">
                 <a href="/" className="-m-1.5 p-1.5">
                     <img className="h-14 w-auto" src={logo} alt="" />
                 </a>
             </div>
-            <div className="flex  lg:hidden">
+            <div className="flex lg:hidden">
                 <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
                     <svg className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -22,7 +22,7 @@ const Navbar = () => {
                 <a href="/" class=" font-semibold leading-6 text-gray-900">About us</a>
                 <a href="/" class=" font-semibold leading-6 text-gray-900">Services</a>
                 <a href="/" class=" font-semibold leading-6 text-gray-900">Portfolio</a>
-                <a href="/" class="font-semibold leading-6 text-gray-900">Contact Us</a>
+                <a href="/" class="font-semibold leading-6 text-gray-900">Contact us</a>
             </div>
 
         </nav>
