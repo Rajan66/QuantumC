feee07f5c40658fd719517235c8df4c8cea96408
Rajan66 <rajanmaharjan042@gmail.com>
2024-04-03 22:40:53 +0545

    fix: added url to contact page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import coverimg from '../images/hero.jpg'
 import Navbar from './Navbar'
+import { Link } from 'react-router-dom'
 
 const Hero = () => {
     return (
@@ -17,12 +18,12 @@ const Hero = () => {
                 </div>
                 <p className="text-medium lg:text-lg">You understand your customers better than anyone. We know that, and we can help you turn that knowledge into results.</p>
                 <div className='flex mt-[50px] justify-center  '>
-                    <button class="border border-black bg-black text-white transition-all hover:bg-white text-lg hover:text-black py-4 px-6 rounded-full mx-10 flex items-center justify-center">
+                    <Link to={'/contact'} class="border border-black bg-black text-white transition-all hover:bg-white text-lg hover:text-black py-4 px-6 rounded-full mx-10 flex items-center justify-center">
                         <span>Create with Us</span>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2" viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M5 10a1 1 0 011-1h6.586l-2.293-2.293a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H6a1 1 0 01-1-1z" clipRule="evenodd" />
                         </svg>
-                    </button>
+                    </Link>
                 </div>
             </div>
         </section>
